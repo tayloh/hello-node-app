0db9bebe6685cc26207b68396b7da60556c5bb81
DESKTOP-CB82KI4\ErikS <erik.stolpe98@gmail.com>
2018-11-20 00:42:52 +0100

    app.js added

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
+
+// https://hackernoon.com/the-definitive-guide-to-express-the-node-js-web-application-framework-649352e2ae87
